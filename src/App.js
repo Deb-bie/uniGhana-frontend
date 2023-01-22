46b7c4bdd53f38c1395accdc8cbb5e98fe6ae037
Debbie <asamoahjaeldeborah@gmail.com>
2023-01-22 05:55:41 +0000

    context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {useContext} from "react"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
+
+  const {user} = useContext(AuthContext);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route 
+          path="/dashboard"
+          element={
+            user !== null 
+            ? <Dashboard />
+            : <Login />
+          }
+        />
       </Routes>
     </Router>
   );
